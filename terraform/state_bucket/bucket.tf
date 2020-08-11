54dbf783e3b55c9e97c240a34b09149c2b8e21f3
Gavin Elder <gavin@improbable.io>
2020-08-11 19:34:07 +0100

    Add Basic TF Bucket

---
+++ terraform/state_bucket/bucket.tf
@@ -0,0 +1,51 @@
+resource "aws_s3_bucket" "bucket" {
+  bucket = "osquery-foundation-terraform-state"
+  acl    = "private"
+  region = "eu-west-2" 
+
+  server_side_encryption_configuration {
+    rule {
+      apply_server_side_encryption_by_default {
+        sse_algorithm = "AES256"
+      }
+    }
+  }
+
+  logging {
+    target_bucket = "bucket_to_go_here"
+    target_prefix = "logs/osquery-terraform-state"
+  }
+
+  versioning {
+    enabled = true
+  }
+
+  lifecycle_rule {
+    enabled = true
+
+    noncurrent_version_transition {
+      days          = 30
+      storage_class = "STANDARD_IA"
+    }
+
+    noncurrent_version_transition {
+      days          = 60
+      storage_class = "GLACIER"
+    }
+
+    noncurrent_version_expiration {
+      days = 90
+    }
+  }
+}
+
+resource "aws_dynamodb_table" "lock_table" {
+  name           = "osquery-foundation-terraform-state"
+  hash_key       = "LockID"
+  read_capacity  = 2
+  write_capacity = 2
+  attribute {
+    name = "LockID"
+    type = "S"
+  }
+}
---
+++ terraform/state_bucket/provider.tf
@@ -0,0 +1,20 @@
+terraform {
+  required_version = "0.13.0"
+
+  # When bootstrapping this, you will have to manually disable this and set the bucket up first.
+  backend "s3" {
+    bucket         = "osquery-foundation-terraform-state"
+    region         = "eu-west-2"
+    key            = "tf/osquery/aws/backend.tfstate"
+    role_arn       = "arn:aws:iam::TBD:role/osquery_tfdeployer"
+    dynamodb_table = "osquery-foundation-terraform-lock"
+  }
+}
+
+provider "aws" {
+  allowed_account_ids = ["11112223333"]
+  assume_role {
+    role_arn = "arn:aws:iam::111122223333:role/osquery_tfdeployer"
+  }
+  region = "eu-west-2"
+}
